039a940c7436c2a2517c0687af4661adf473327f
Abhay-Bharti <117109756+Abhay-Bharti@users.noreply.github.com>
2022-12-01 20:38:39 +0530

    Delete class1-functions_on_backlash-CipherSchools.py

--- class1-functions_on_backlash-CipherSchools.py
+++
@@ -1,6 +0,0 @@
-print("this is \\\\ double backslash")
-print("These are /\\/\\/\\/\\ big mountains")
-print("he is a \tgood singer")
-print(" \\\" \\n \\t \\\' ")
-# r makes the escape sequence as normal text.
-print(r"line a \" line B")
